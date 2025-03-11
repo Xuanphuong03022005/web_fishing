20fbed3c9705cf7585b77c9132943a64abbb270b
Pham Xuan Phuong <pp2215006@gmail.com>
2025-03-11 21:18:52 +0700

    B

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -1,11 +1,7 @@
     <?php
 class HomeController{
     public static function index() {
-<<<<<<< HEAD
      $a = 506;
-=======
-     $a = 807;
->>>>>>> afa01fcaf8c67d831a07b7c8459806fe997e00ae
         
         $text = "Xuân Phương";
         require_once 'view/home.php';
